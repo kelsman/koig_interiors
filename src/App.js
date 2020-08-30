63b5981ca1352877348afad54a3dd9a077cff8df
kelvin <osekelvin22@gmail.com>
2020-08-31 00:41:37 +0100

    removed lazy loader and added reveal animation to footer

--- src/App.js
+++ src/App.js
@@ -1,41 +1,29 @@
-import React, { Component, lazy, Suspense } from 'react';
+import React, { Component } from 'react';
 import Header from './components/header'
 import Footer from './components/footer/index';
 import { Route, Switch, Redirect } from 'react-router-dom';
 //pages
 
-
 import Homepage from './pages/homepage';
-
-//import LogInPage from './pages/login-page';
-//import SignUpPage from './pages/signup-page';
-//import CartCheckoutPage from './pages/cart-checkout';
-//import FurniturePage from './pages/furniture-page';
-//import OutdoorPage from './pages/outdoor-page';
-//import RugPage from './pages/rug-page';
-//import LighteningPage from './pages/lightening-page';
-//import PaymentPage from './pages/payment-page'
+import LogInPage from './pages/login-page';
+import SignUpPage from './pages/signup-page';
+import CartCheckoutPage from './pages/cart-checkout';
+import FurniturePage from './pages/furniture-page';
+import OutdoorPage from './pages/outdoor-page';
+import RugPage from './pages/rug-page';
+import LighteningPage from './pages/lightening-page';
+import PaymentPage from './pages/payment-page'
+import Chairs from './pages/chairs';
+import ForgetPassword from './pages/forget-password';
+import ContactPage from './pages/contact-page';
 //firebase
 //redux
 import { auth, createUserProfile } from './firebase/utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './Redux/user/userActions';
-//import Chairs from './pages/chairs';
-//import ForgetPassword from './pages/forget-password';
-//import ContactPage from './pages/contact-page';
 
-//const Homepage = lazy(() => import('./pages/homepage'));
-const LogInPage = lazy(() => import('./pages/login-page'));
-const SignUpPage = lazy(() => import('./pages/signup-page'));
-const CartCheckoutPage = lazy(() => import('./pages/cart-checkout'));
-const FurniturePage = lazy(() => import('./pages/furniture-page'));
-const OutdoorPage = lazy(() => import('./pages/outdoor-page'));
-const RugPage = lazy(() => import('./pages/rug-page'));
-const LighteningPage = lazy(() => import('./pages/lightening-page'));
-const PaymentPage = lazy(() => import('./pages/payment-page'));
-const Chairs = lazy(() => import('./pages/chairs'));
-const ForgetPassword = lazy(() => import('./pages/forget-password'));
-const ContactPage = lazy(() => import('./pages/contact-page'));
+
+
 
 
 class App extends Component {
@@ -90,17 +78,17 @@ class App extends Component {
             <Route path='/signup' render={() =>
               currentUser ? <Redirect to='/' /> : <SignUpPage />
             } />
-            <Suspense fallback={<div> Loading...</div>}>
-              <Route exact path="/accent-chairs" render={() => <Chairs />} />
-              <Route exact path='/furniture' render={() => <FurniturePage />} />
-              <Route exact path="/checkout-page" render={() => < CartCheckoutPage />} />
-              <Route exact path='/outdoor' render={() => <OutdoorPage />} />
-              <Route exact path='/rugs' component={RugPage} />
-              <Route exact path='/lightening' component={LighteningPage} />
-              <Route exact path='/payment' component={PaymentPage} />
-              <Route exact path="/password-reset" component={ForgetPassword} />
-              <Route exaact path="/contact-us" component={ContactPage} />
-            </Suspense>
+
+            <Route exact path="/accent-chairs" render={() => <Chairs />} />
+            <Route exact path='/furniture' render={() => <FurniturePage />} />
+            <Route exact path="/checkout-page" render={() => < CartCheckoutPage />} />
+            <Route exact path='/outdoor' render={() => <OutdoorPage />} />
+            <Route exact path='/rugs' component={RugPage} />
+            <Route exact path='/lightening' component={LighteningPage} />
+            <Route exact path='/payment' component={PaymentPage} />
+            <Route exact path="/password-reset" component={ForgetPassword} />
+            <Route exaact path="/contact-us" component={ContactPage} />
+
           </Switch>
         </div>
 
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,70 +3,74 @@ import { Link } from 'react-router-dom';
 import './style.scss';
 import Mastercard from '../../assets/mastercard.png';
 import Verve from '../../assets/verve.png';
-import Visa from '../../assets/visa.png'
+import Visa from '../../assets/visa.png';
+import { Fade } from "react-awesome-reveal";
 class Footer extends Component {
     render() {
         return (
+            <Fade>
 
-            <footer>
-                <div className="main-footer">
+                <footer className="animate__slideInUp">
 
-
-                    <div className="social">
-
-                        <ul>
-
-                            <li>
-                                <a href='https://twitter.com/KOigiangbe'>
-                                    <i className="fab fa-twitter"></i>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://github.com/kelwaffi">
-                                    <i className="fab fa-github"></i>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="https://www.instagram.com/kels_man/">
-                                    <i className="fab fa-instagram"></i>
-
-                                </a>
-                            </li>
+                    <div className="main-footer">
 
 
-                        </ul>
+                        <div className="social">
 
+                            <ul>
+
+                                <li>
+                                    <a href='https://twitter.com/KOigiangbe'>
+                                        <i className="fab fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://github.com/kelwaffi">
+                                        <i className="fab fa-github"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.instagram.com/kels_man/">
+                                        <i className="fab fa-instagram"></i>
+
+                                    </a>
+                                </li>
+
+
+                            </ul>
+
+                        </div>
+                        <div className="others">
+
+                            <ul className="about">
+                                <h4> About Koig_interior</h4>
+                                <li>
+                                    <Link to='/contact-us'> Contact us </Link>
+                                </li>
+                                <li> <Link to='/about-us'> About us</Link></li>
+                                <li> <Link to='/'> Terms and conditions</Link></li>
+
+                            </ul>
+
+                            <ul className="payment">
+                                <h4> payment</h4>
+                                <li> <img src={Mastercard} alt="" height="30px" width="60px" /> </li>
+                                <li> <img src={Visa} alt="" height="30px" width="60px" /> </li>
+                                <li> <img src={Verve} alt="" height="30px" width="60px" /> </li>
+
+                            </ul>
+
+                        </div>
                     </div>
-                    <div className="others">
 
-                        <ul className="about">
-                            <h4> About Koig_interior</h4>
-                            <li>
-                                <Link to='/contact-us'> Contact us </Link>
-                            </li>
-                            <li> <Link to='/about-us'> About us</Link></li>
-                            <li> <Link to='/'> Terms and conditions</Link></li>
+                    <div className="bottom-footer">
+                        <p
 
-                        </ul>
-
-                        <ul className="payment">
-                            <h4> payment</h4>
-                            <li> <img src={Mastercard} alt="" height="30px" width="60px" /> </li>
-                            <li> <img src={Visa} alt="" height="30px" width="60px" /> </li>
-                            <li> <img src={Verve} alt="" height="30px" width="60px" /> </li>
-
-                        </ul>
-
+                        >  copyright &copy; 2020 Mazi_Kels. All right reserved</p>
                     </div>
-                </div>
 
-                <div className="bottom-footer">
-                    <p
-
-                    >  copyright &copy; 2020 Mazi_Kels. All right reserved</p>
-                </div>
-
-            </footer>
+                </footer>
+            </Fade>
         )
     }
 }
